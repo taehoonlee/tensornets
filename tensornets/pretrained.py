1a943c89c33fd01bef7e9e09580d3ae19db82737
Taehoon Lee <me@taehoonlee.com>
2017-10-08 17:31:56 +0900

    Add and clean up docs for pretrained weights

--- tensornets/pretrained.py
+++ tensornets/pretrained.py
@@ -1,3 +1,18 @@
+"""Collection of pretrained models.
+
+This module provides loading functions for pre-trained weights.
+All the weight files are converted as a Keras-like format that
+serializes every single tensor from the following repositories:
+
+[1]: https://github.com/BVLC/caffe/tree/master/models/bvlc_googlenet
+     "BAIR Caffe Model Zoo"
+[2]: https://github.com/fchollet/keras/tree/master/keras/applications
+     "Keras"
+[3]: https://github.com/KaimingHe/deep-residual-networks
+     "Caffe ResNets"
+[4]: https://github.com/facebook/fb.resnet.torch
+     "Torch ResNets"
+"""
 from __future__ import absolute_import
 
 from .utils import get_file
@@ -13,8 +28,7 @@ __model_url__ = 'https://github.com/taehoonlee/deep-learning-models/' \
 
 
 def load_inception1(scopes):
-    # Converted from the BAIR's Caffe Model Zoo as a Keras format
-    # Refer to https://github.com/BVLC/caffe/tree/master/models/bvlc_googlenet
+    """Converted from the [BAIR Caffe Model Zoo][1]."""
     filename = 'inception1.h5'
     weights_path = get_file(
         filename, __model_url__ + 'inception/' + filename,
@@ -24,8 +38,7 @@ def load_inception1(scopes):
 
 
 def load_inception3(scopes):
-    # Copied from keras with a slight modification due to the order of weights
-    # Refer to https://github.com/fchollet/keras/tree/master/keras/applications
+    """Copied from [keras][2] with modifications on the order of weights."""
     filename = 'inception3.h5'
     weights_path = get_file(
         filename, __model_url__ + 'inception/' + filename,
@@ -35,8 +48,7 @@ def load_inception3(scopes):
 
 
 def load_resnet50(scopes):
-    # Converted from the original Caffe files as a Keras format
-    # Refer to https://github.com/KaimingHe/deep-residual-networks
+    """Converted from the original [Caffe ResNets][3]."""
     filename = 'resnet50.h5'
     weights_path = get_file(
         filename, __model_url__ + 'resnet/' + filename,
@@ -46,8 +58,7 @@ def load_resnet50(scopes):
 
 
 def load_resnet101(scopes):
-    # Converted from the original Caffe files as a Keras format
-    # Refer to https://github.com/KaimingHe/deep-residual-networks
+    """Converted from the original [Caffe ResNets][3]."""
     filename = 'resnet101.h5'
     weights_path = get_file(
         filename, __model_url__ + 'resnet/' + filename,
@@ -57,8 +68,7 @@ def load_resnet101(scopes):
 
 
 def load_resnet152(scopes):
-    # Converted from the original Caffe files as a Keras format
-    # Refer to https://github.com/KaimingHe/deep-residual-networks
+    """Converted from the original [Caffe ResNets][3]."""
     filename = 'resnet152.h5'
     weights_path = get_file(
         filename, __model_url__ + 'resnet/' + filename,
@@ -68,41 +78,38 @@ def load_resnet152(scopes):
 
 
 def load_keras_resnet50(scopes):
-    # Copied from keras
-    # Refer to https://github.com/fchollet/keras/tree/master/keras/applications
+    """Copied from [keras][2]."""
     filename = 'resnet50_weights_tf_dim_ordering_tf_kernels.h5'
     weights_path = get_file(
         filename, __keras_url__ + filename,
         cache_subdir='models',
         md5_hash='a7b3fe01876f51b976af0dea6bc144eb')
-    move_rules = [
-        ('bn2a_branch2c', -1),
-        ('bn3a_branch2c', -1),
-        ('bn4a_branch2c', -1),
-        ('bn5a_branch2c', -1),
-        ('res2a_branch1', -6),
-        ('res3a_branch1', -6),
-        ('res4a_branch1', -6),
-        ('res5a_branch1', -6),
-        ('bn2a_branch1', -6),
-        ('bn3a_branch1', -6),
-        ('bn4a_branch1', -6),
-        ('bn5a_branch1', -6),
-    ]
+    move_rules = []
+    for i in range(2, 6):
+        move_rules.append(("bn%da_branch2c" % i, -1))
+        move_rules.append(("res%da_branch1" % i, -6))
+        move_rules.append(("bn%da_branch1" % i, -6))
     return load_keras_weights(scopes, weights_path, move_rules)
 
 
+move_rules_fb_resnet_torch = []
+for i in range(4, 8):
+    move_rules_fb_resnet_torch.append(("%d.0.0.1.0.weight" % i, -18))
+    move_rules_fb_resnet_torch.append(("%d.0.0.1.0.bias" % i, -18))
+    move_rules_fb_resnet_torch.append(("%d.0.0.1.1.weight" % i, -18))
+    move_rules_fb_resnet_torch.append(("%d.0.0.1.1.bias" % i, -18))
+    move_rules_fb_resnet_torch.append(("%d.0.0.1.1.running_mean" % i, -18))
+    move_rules_fb_resnet_torch.append(("%d.0.0.1.1.running_var" % i, -18))
+
+
 def load_torch_resnet101(scopes):
-    weights_path = '/home/taehoonlee/Data/torch-ResNet/resnet_101_cpu.pth'
-    move_rules = []
-    for i in range(4, 8):
-        move_rules.append(("%d.0.0.1.0.weight" % i, -18))
-        move_rules.append(("%d.0.0.1.0.bias" % i, -18))
-        move_rules.append(("%d.0.0.1.1.weight" % i, -18))
-        move_rules.append(("%d.0.0.1.1.bias" % i, -18))
-        move_rules.append(("%d.0.0.1.1.running_mean" % i, -18))
-        move_rules.append(("%d.0.0.1.1.running_var" % i, -18))
-    return load_torch_weights(scopes, weights_path, move_rules)
+    """Converted from the [Torch ResNets][4]."""
+    filename = 'resnet_101_cpu.pth'
+    weights_path = get_file(
+        filename, __model_url__ + 'resnet/' + filename,
+        cache_subdir='models',
+        md5_hash='cb3f0ac4687cb63d5f0861d651da844b')
+    return load_torch_weights(scopes, weights_path, move_rules_fb_resnet_torch)
 
 
 # Simple alias.
