7340666c47d75aa6d8595500722c3a8bbe58f2b1
Taehoon Lee <me@taehoonlee.com>
2018-01-24 19:15:18 +0900

    Speed up basic tests with random selection

--- tests/basics_test.py
+++ tests/basics_test.py
@@ -4,21 +4,28 @@ import numpy as np
 import tensorflow as tf
 import tensornets as nets
 import pytest
+import random
 
 from distutils.version import LooseVersion
 
 
 @pytest.mark.parametrize('net,shape', [
-    (nets.ResNet50, (224, 224, 3)),
-    (nets.ResNet101, (224, 224, 3)),
-    (nets.ResNet152, (224, 224, 3)),
-    (nets.ResNet50v2, (299, 299, 3)),
-    (nets.ResNet101v2, (299, 299, 3)),
-    (nets.ResNet152v2, (299, 299, 3)),
+    random.choice([
+        (nets.ResNet50, (224, 224, 3)),
+        (nets.ResNet101, (224, 224, 3)),
+        (nets.ResNet152, (224, 224, 3)),
+    ]),
+    random.choice([
+        (nets.ResNet50v2, (299, 299, 3)),
+        (nets.ResNet101v2, (299, 299, 3)),
+        (nets.ResNet152v2, (299, 299, 3)),
+    ]),
     (nets.ResNet200v2, (224, 224, 3)),
-    (nets.ResNeXt50, (224, 224, 3)),
-    (nets.ResNeXt101, (224, 224, 3)),
-    (nets.ResNeXt101c64, (224, 224, 3)),
+    random.choice([
+        (nets.ResNeXt50, (224, 224, 3)),
+        (nets.ResNeXt101, (224, 224, 3)),
+        (nets.ResNeXt101c64, (224, 224, 3)),
+    ]),
     (nets.WideResNet50, (224, 224, 3)),
     (nets.Inception1, (224, 224, 3)),
     (nets.Inception2, (224, 224, 3)),
@@ -35,13 +42,17 @@ from distutils.version import LooseVersion
         marks=pytest.mark.xfail(
             LooseVersion(tf.__version__) < LooseVersion('1.3.0'),
             reason='NASNetAmobile requires TensorFlow >= 1.3.0')),
-    (nets.DenseNet121, (224, 224, 3)),
-    (nets.DenseNet169, (224, 224, 3)),
-    (nets.DenseNet201, (224, 224, 3)),
-    (nets.MobileNet25, (224, 224, 3)),
-    (nets.MobileNet50, (224, 224, 3)),
-    (nets.MobileNet75, (224, 224, 3)),
-    (nets.MobileNet100, (224, 224, 3)),
+    random.choice([
+        (nets.DenseNet121, (224, 224, 3)),
+        (nets.DenseNet169, (224, 224, 3)),
+        (nets.DenseNet201, (224, 224, 3)),
+    ]),
+    random.choice([
+        (nets.MobileNet25, (224, 224, 3)),
+        (nets.MobileNet50, (224, 224, 3)),
+        (nets.MobileNet75, (224, 224, 3)),
+        (nets.MobileNet100, (224, 224, 3)),
+    ]),
     (nets.SqueezeNet, (224, 224, 3)),
 ])
 def test_basics(net, shape):
