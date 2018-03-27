23f955bd35adf01d33f413a6d6b37b80d680796a
Taehoon Lee <me@taehoonlee.com>
2018-03-27 10:05:24 +0900

    Prepare 0.3.0 release

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='tensornets',
-      version='0.2.0',
+      version='0.3.0',
       description='high level network definitions in tensorflow',
       author='Taehoon Lee',
       author_email='me@taehoonlee.com',
       url='https://github.com/taehoonlee/tensornets',
-      download_url='https://github.com/taehoonlee/tensornets/tarball/0.2.0',
+      download_url='https://github.com/taehoonlee/tensornets/tarball/0.3.0',
       license='MIT',
       install_requires=['tensorflow'],
       packages=['tensornets', 'tensornets.datasets', 'tensornets.references'],
