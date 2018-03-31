70c2c8c745ea4f46bf8735867c33873991e43975
Taehoon Lee <me@taehoonlee.com>
2018-03-31 21:46:31 +0900

    Revise `get_boxes` to prepare YOLOv3

--- tensornets/references/yolo_utils.py
+++ tensornets/references/yolo_utils.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import os
 import numpy as np
 
@@ -19,6 +21,9 @@ with open(os.path.join(os.path.dirname(__file__), 'voc.names'), 'r') as f:
     labels_voc = [line.rstrip() for line in f.readlines()]
 
 bases = dict()
+bases['yolov3'] = {'anchors': [10, 13, 16, 30, 33, 23, 30, 61, 62, 45,
+                               59, 119, 116, 90, 156, 198, 373, 326]}
+bases['yolov3voc'] = bases['yolov3']
 bases['yolov2'] = {'anchors': [0.57273, 0.677385, 1.87446, 2.06253, 3.33843,
                                5.47434, 7.88282, 3.52778, 9.77052, 9.16828]}
 bases['yolov2voc'] = {'anchors': [1.3221, 1.73145, 3.19275, 4.00944, 5.05587,
@@ -29,6 +34,7 @@ bases['tinyyolov2voc'] = {'anchors': [1.08, 1.19, 3.42, 4.41, 6.63,
 
 def opts(model_name, t=0.1):
     opt = bases[model_name].copy()
+    opt.update({'num': len(opt['anchors']) // 2})
     if 'voc' in model_name:
         opt.update({'classes': len(labels_voc), 'labels': labels_voc})
     else:
@@ -36,11 +42,10 @@ def opts(model_name, t=0.1):
     return opt
 
 
-def get_boxes(opts, outs, source_size, num=5, threshold=0.1):
+def get_boxes(opts, outs, source_size, threshold=0.1):
     assert box_constructor is not None, '`get_boxes` requires `darkflow`.'
     h, w = source_size
     boxes = [[] for _ in xrange(opts['classes'])]
-    opts['num'] = num
     opts['thresh'] = threshold
     opts['out_size'] = list(outs[0].shape)
     results = box_constructor(opts, outs[0].copy())
