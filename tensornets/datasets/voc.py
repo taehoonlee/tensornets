2ff227cbd54c269f6e2c7c46c768c6976dc10539
Taehoon Lee <me@taehoonlee.com>
2018-05-14 10:22:40 +0900

    Add training data loader for VOC

--- tensornets/datasets/voc.py
+++ tensornets/datasets/voc.py
@@ -1,13 +1,17 @@
 """Collection of PASCAL VOC utils
 
-The codes were refactored from the original py-faster-rcnn
-(https://github.com/rbgirshick/py-faster-rcnn/blob/master/lib/
-datasets/voc_eval.py). Especially, each part was from the following:
+The codes were refactored from [py-faster-rcnn](https://github.com/
+rbgirshick/py-faster-rcnn/blob/master/lib/datasets/voc_eval.py) and
+[darkflow](https://github.com/thtrieu/darkflow/blob/master/darkflow/
+net/yolov2/data.py). Especially, each part was from the following:
 
-1. get_annotations: parse_rec
-2. evaluate_class: voc_ap
-3. evaluate: voc_eval
+1. get_annotations: parse_rec in py-faster-rcnn
+2. evaluate_class: voc_ap in py-faster-rcnn
+3. evaluate: voc_eval in py-faster-rcnn
+4. load_train: _batch in darkflow
 """
+from __future__ import division
+
 import os
 import numpy as np
 import xml.etree.ElementTree as ET
@@ -21,6 +25,11 @@ try:
     xrange          # Python 2
 except NameError:
     xrange = range  # Python 3
+
+try:
+    reduce
+except NameError:
+    from functools import reduce
 
 
 with open(os.path.join(os.path.dirname(__file__), 'voc.names'), 'r') as f:
@@ -188,3 +197,107 @@ def evaluate(results, data_dir, data_name, ovthresh=0.5, verbose=True):
 
     strs += "Mean = %.4f" % np.mean(aps)
     return strs
+
+
+def load_train(data_dir, data_name,
+               batch_size=64, shuffle=True,
+               target_size=416, anchors=5, classes=20,
+               total_num=None, dtype=np.float32):
+    assert cv2 is not None, '`load_train` requires `cv2`.'
+    if isinstance(data_dir, list):
+        files = []
+        annotations = {}
+        for d in data_dir:
+            files.append(get_files(d, data_name, total_num))
+            annotations.update(get_annotations(d, files[-1]))
+        dirs = np.concatenate([i * np.ones(len(f), dtype=np.int)
+                               for (i, f) in enumerate(files)])
+        files = reduce(lambda x, y: x + y, files)
+    else:
+        files = get_files(data_dir, data_name, total_num)
+        annotations = get_annotations(data_dir, files)
+        dirs = np.zeros(len(files), dtype=np.int)
+
+    total_num = len(files)
+    for f in files:
+        annotations[f] = reduce(lambda x, y: x + y, annotations[f])
+
+    if isinstance(target_size, int):
+        target_size = (target_size, target_size)
+    feature_size = [x // 32 for x in target_size]
+    cells = feature_size[0] * feature_size[1]
+
+    b = 0
+    while True:
+        if b == 0:
+            if shuffle is True:
+                idx = np.random.permutation(total_num)
+            else:
+                idx = np.arange(total_num)
+        if b + batch_size > total_num:
+            b = 0
+            yield None, None
+        else:
+            batch_num = batch_size
+
+        imgs = np.zeros((batch_num,) + target_size + (3,), dtype=dtype)
+        probs = np.zeros((batch_num, cells, anchors, classes), dtype=dtype)
+        confs = np.zeros((batch_num, cells, anchors), dtype=dtype)
+        coord = np.zeros((batch_num, cells, anchors, 4), dtype=dtype)
+        proid = np.zeros((batch_num, cells, anchors, classes), dtype=dtype)
+        prear = np.zeros((batch_num, cells, 4), dtype=dtype)
+        areas = np.zeros((batch_num, cells, anchors), dtype=dtype)
+        upleft = np.zeros((batch_num, cells, anchors, 2), dtype=dtype)
+        botright = np.zeros((batch_num, cells, anchors, 2), dtype=dtype)
+
+        for i in range(batch_num):
+            d = data_dir[dirs[idx[b + i]]]
+            f = files[idx[b + i]]
+            x = cv2.imread("%s/JPEGImages/%s.jpg" % (d, f))
+            h, w = x.shape[:2]
+            cellx = 1. * w / feature_size[1]
+            celly = 1. * h / feature_size[0]
+
+            processed_objs = []
+            for obj in annotations[f]:
+                bbox = obj['bbox']
+                centerx = .5 * (bbox[0] + bbox[2])  # xmin, xmax
+                centery = .5 * (bbox[1] + bbox[3])  # ymin, ymax
+                cx = centerx / cellx
+                cy = centery / celly
+                if cx >= feature_size[1] or cy >= feature_size[0]:
+                    continue
+                processed_objs += [[
+                    classidx(obj['name']),
+                    cx - np.floor(cx),  # centerx
+                    cy - np.floor(cy),  # centery
+                    np.sqrt(float(bbox[2] - bbox[0]) / w),
+                    np.sqrt(float(bbox[3] - bbox[1]) / h),
+                    int(np.floor(cy) * feature_size[1] + np.floor(cx))
+                ]]
+
+            # Calculate placeholders' values
+            for obj in processed_objs:
+                probs[i, obj[5], :, :] = [[0.] * classes] * anchors
+                probs[i, obj[5], :, obj[0]] = 1.
+                proid[i, obj[5], :, :] = [[1.] * classes] * anchors
+                coord[i, obj[5], :, :] = [obj[1:5]] * anchors
+                prear[i, obj[5], 0] = obj[1] - obj[3]**2 * .5 * feature_size[1]
+                prear[i, obj[5], 1] = obj[2] - obj[4]**2 * .5 * feature_size[0]
+                prear[i, obj[5], 2] = obj[1] + obj[3]**2 * .5 * feature_size[1]
+                prear[i, obj[5], 3] = obj[2] + obj[4]**2 * .5 * feature_size[0]
+                confs[i, obj[5], :] = [1.] * anchors
+
+            # Finalise the placeholders' values
+            ul = np.expand_dims(prear[i, :, 0:2], 1)
+            br = np.expand_dims(prear[i, :, 2:4], 1)
+            wh = br - ul
+            area = wh[:, :, 0] * wh[:, :, 1]
+            upleft[i, :, :, :] = np.concatenate([ul] * anchors, 1)
+            botright[i, :, :, :] = np.concatenate([br] * anchors, 1)
+            areas[i, :, :] = np.concatenate([area] * anchors, 1)
+
+            imgs[i] = cv2.resize(x, target_size,
+                                 interpolation=cv2.INTER_LINEAR)
+        yield imgs, [probs, confs, coord, proid, areas, upleft, botright]
+        b += batch_size
