8b455a9a490aa209a6a13c1aae7ba19bfb92cf6b
Taehoon Lee <me@taehoonlee.com>
2019-03-01 18:54:04 +0900

    Add tests for whether the desired list is returned

--- tests/basics_test.py
+++ tests/basics_test.py
@@ -17,68 +17,68 @@ pytestmark = pytest.mark.skipif(
     reason='Runs only when the relevant files have been modified.')
 
 
-@pytest.mark.parametrize('net,shape', [
+@pytest.mark.parametrize('net,shape,weights,outputs,middles', [
     random.choice([
-        (nets.ResNet50, (224, 224, 3)),
-        (nets.ResNet101, (224, 224, 3)),
-        (nets.ResNet152, (224, 224, 3)),
+        (nets.ResNet50, (224, 224, 3), 320, 161, 16),
+        (nets.ResNet101, (224, 224, 3), 626, 314, 33),
+        (nets.ResNet152, (224, 224, 3), 932, 467, 50),
     ]),
     random.choice([
-        (nets.ResNet50v2, (299, 299, 3)),
-        (nets.ResNet101v2, (299, 299, 3)),
-        (nets.ResNet152v2, (299, 299, 3)),
+        (nets.ResNet50v2, (299, 299, 3), 272, 192, 16),
+        (nets.ResNet101v2, (299, 299, 3), 544, 379, 33),
+        (nets.ResNet152v2, (299, 299, 3), 816, 566, 50),
     ]),
-    (nets.ResNet200v2, (224, 224, 3)),
+    (nets.ResNet200v2, (224, 224, 3), 1224, 745, 66),
     random.choice([
-        (nets.ResNeXt50, (224, 224, 3)),
-        (nets.ResNeXt101, (224, 224, 3)),
-        (nets.ResNeXt101c64, (224, 224, 3)),
+        (nets.ResNeXt50, (224, 224, 3), 267, 193, 16),
+        (nets.ResNeXt101, (224, 224, 3), 522, 380, 33),
+        (nets.ResNeXt101c64, (224, 224, 3), 522, 380, 33),
     ]),
-    (nets.WideResNet50, (224, 224, 3)),
-    (nets.Inception1, (224, 224, 3)),
-    (nets.Inception2, (224, 224, 3)),
-    (nets.Inception3, (299, 299, 3)),
-    (nets.Inception4, (299, 299, 3)),
-    (nets.InceptionResNet2, (299, 299, 3)),
+    (nets.WideResNet50, (224, 224, 3), 267, 177, 16),
+    (nets.Inception1, (224, 224, 3), 116, 143, 11),
+    (nets.Inception2, (224, 224, 3), 277, 231, 10),
+    (nets.Inception3, (299, 299, 3), 378, 313, 11),
+    (nets.Inception4, (299, 299, 3), 598, 494, 17),
+    (nets.InceptionResNet2, (299, 299, 3), 898, 744, 43),
     pytest.param(
-        nets.NASNetAlarge, (331, 331, 3),
+        nets.NASNetAlarge, (331, 331, 3), 1558, 1029, 20,
         marks=pytest.mark.xfail(
             LooseVersion(tf.__version__) < LooseVersion('1.3.0'),
             reason='NASNetAlarge requires TensorFlow >= 1.3.0')),
     pytest.param(
-        nets.NASNetAmobile, (224, 224, 3),
+        nets.NASNetAmobile, (224, 224, 3), 1138, 759, 14,
         marks=pytest.mark.xfail(
             LooseVersion(tf.__version__) < LooseVersion('1.3.0'),
             reason='NASNetAmobile requires TensorFlow >= 1.3.0')),
     pytest.param(
-        nets.PNASNetlarge, (331, 331, 3),
+        nets.PNASNetlarge, (331, 331, 3), 1179, 752, 12,
         marks=pytest.mark.xfail(
             LooseVersion(tf.__version__) < LooseVersion('1.3.0'),
             reason='PNASNetlarge requires TensorFlow >= 1.3.0')),
     random.choice([
-        (nets.VGG16, (224, 224, 3)),
-        (nets.VGG19, (224, 224, 3)),
+        (nets.VGG16, (224, 224, 3), 32, 40, 9),
+        (nets.VGG19, (224, 224, 3), 38, 46, 12),
     ]),
     random.choice([
-        (nets.DenseNet121, (224, 224, 3)),
-        (nets.DenseNet169, (224, 224, 3)),
-        (nets.DenseNet201, (224, 224, 3)),
+        (nets.DenseNet121, (224, 224, 3), 606, 429, 61),
+        (nets.DenseNet169, (224, 224, 3), 846, 597, 85),
+        (nets.DenseNet201, (224, 224, 3), 1006, 709, 101),
     ]),
     random.choice([
-        (nets.MobileNet25, (224, 224, 3)),
-        (nets.MobileNet50, (224, 224, 3)),
-        (nets.MobileNet75, (224, 224, 3)),
-        (nets.MobileNet100, (224, 224, 3)),
+        (nets.MobileNet25, (224, 224, 3), 137, 85, 11),
+        (nets.MobileNet50, (224, 224, 3), 137, 85, 11),
+        (nets.MobileNet75, (224, 224, 3), 137, 85, 11),
+        (nets.MobileNet100, (224, 224, 3), 137, 85, 11),
     ]),
     random.choice([
-        (nets.MobileNet35v2, (224, 224, 3)),
-        (nets.MobileNet50v2, (224, 224, 3)),
-        (nets.MobileNet75v2, (224, 224, 3)),
-        (nets.MobileNet100v2, (224, 224, 3)),
-        (nets.MobileNet130v2, (224, 224, 3)),
-        (nets.MobileNet140v2, (224, 224, 3)),
+        (nets.MobileNet35v2, (224, 224, 3), 262, 152, 62),
+        (nets.MobileNet50v2, (224, 224, 3), 262, 152, 62),
+        (nets.MobileNet75v2, (224, 224, 3), 262, 152, 62),
+        (nets.MobileNet100v2, (224, 224, 3), 262, 152, 62),
+        (nets.MobileNet130v2, (224, 224, 3), 262, 152, 62),
+        (nets.MobileNet140v2, (224, 224, 3), 262, 152, 62),
     ]),
-    (nets.SqueezeNet, (224, 224, 3)),
+    (nets.SqueezeNet, (224, 224, 3), 52, 66, 10),
 ], ids=[
     'ResNet',
     'ResNetv2',
@@ -99,11 +99,18 @@ pytestmark = pytest.mark.skipif(
     'MobileNetv2',
     'SqueezeNet',
 ])
-def test_classification_basics(net, shape):
+def test_classification_basics(net, shape, weights, outputs, middles):
     with tf.Graph().as_default():
         inputs = tf.placeholder(tf.float32, [None] + list(shape))
         model = net(inputs, is_training=False)
         assert isinstance(model, tf.Tensor)
+
+        # Temporary disable tests whether the desired list is returned
+        # for TensorFlow == 1.1.0 or 1.2.0
+        if LooseVersion(tf.__version__) > LooseVersion('1.2.0'):
+            assert len(model.get_weights()) == weights
+            assert len(model.get_outputs()) == outputs
+            assert len(model.get_middles()) == middles
 
         x = np.random.random((1,) + shape).astype(np.float32) * 255
 
