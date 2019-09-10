9b0b82a0d79373d3217085b8567e9ee362b8e2ee
Taehoon Lee <me@taehoonlee.com>
2019-09-10 17:45:39 +0900

    Add evaluation codes for all imagenet models

---
+++ examples/evaluate_imagenet.py
@@ -0,0 +1,142 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import tensornets as nets
+
+from imagenet_preprocessing import input_fn as _input_fn
+
+
+tf.app.flags.DEFINE_integer(
+    'log_every_n_steps', 50,
+    'The frequency with which logs are print.')
+
+tf.app.flags.DEFINE_integer(
+    'batch_size', 200, 'The number of samples in each batch.')
+
+tf.app.flags.DEFINE_integer(
+    'steps', None, 'The number of steps for evaluation.')
+
+tf.app.flags.DEFINE_string(
+    'checkpoint_path', None,
+    'The directory where the model was written to or an absolute path to a '
+    'checkpoint file.')
+
+tf.app.flags.DEFINE_string(
+    'model_name', 'ResNet50', 'The name of the architecture to evaluate.')
+
+tf.app.flags.DEFINE_string(
+    'dataset_dir', '/home/taehoonlee/Data/imagenet/tfrecords',
+    'The directory where the dataset files are stored.')
+
+tf.app.flags.DEFINE_integer(
+    'eval_image_size', 224, 'The eval image size')
+
+tf.app.flags.DEFINE_integer(
+    'normalize', 0, 'The normalization type')
+
+FLAGS = tf.app.flags.FLAGS
+
+
+# Simple trick to suppress the warning
+# "It seems that global step has not been increased"
+class hook(tf.train.StepCounterHook):
+    def __init__(self, every_n_steps):
+        self._steps = 0
+        super(hook, self).__init__(every_n_steps)
+
+    def after_run(self, run_context, run_values):
+        self._steps += 1
+        if self._timer.should_trigger_for_step(self._steps):
+            t, steps = self._timer.update_last_triggered_step(self._steps)
+            if t is not None:
+                tf.logging.info("%g secs per step on average", t / steps)
+
+
+def input_fn():
+    return _input_fn(
+        is_training=False,
+        data_dir=FLAGS.dataset_dir,
+        batch_size=FLAGS.batch_size,
+        eval_image_size=FLAGS.eval_image_size,
+        normalize=FLAGS.normalize)
+
+
+def model_fn(features, labels, mode):
+    models = []
+    logits = []
+    classes = []
+    init_op = [tf.train.get_or_create_global_step().initializer]
+    for (i, model_name) in enumerate(FLAGS.model_name.split(',')):
+        with tf.device("/gpu:%d" % i):
+            network_fn = getattr(nets, model_name)
+            models.append(network_fn(features, is_training=False))
+            logits.append(models[i].get_outputs()[-2])
+            classes.append(tf.argmax(logits[i], axis=1))
+            if FLAGS.checkpoint_path is None:
+                init_op.extend(models[i].pretrained())
+
+    scaffold = None
+    if FLAGS.checkpoint_path is None:
+        scaffold = tf.train.Scaffold(init_op=init_op)
+
+    loss = []
+    for i in range(len(models)):
+        cross_entropy = tf.losses.sparse_softmax_cross_entropy(
+            logits=logits[i], labels=labels)
+        loss.append(cross_entropy)
+    loss = tf.reduce_sum(loss)
+
+    metrics = None
+    if mode == tf.estimator.ModeKeys.EVAL:
+        metrics = {}
+        for i in range(len(models)):
+            top1 = tf.metrics.accuracy(labels=labels, predictions=classes[i])
+            top5 = tf.contrib.metrics.streaming_sparse_recall_at_k(
+                logits[i], tf.cast(labels, tf.int64), k=5)
+            size = sum([w.shape.num_elements()
+                        for w in models[i].get_weights()])
+            metrics.update({"%dTop1" % i: top1,
+                            "%dTop5" % i: top5,
+                            "%dSize" % i: (tf.constant(size), tf.no_op())})
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        scaffold=scaffold,
+        predictions=None,
+        loss=loss,
+        train_op=None,
+        eval_metric_ops=metrics,
+        export_outputs=None)
+
+
+def main(_):
+    if not FLAGS.dataset_dir:
+        raise ValueError('You must supply the dataset directory.')
+
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    classifier = tf.estimator.Estimator(
+        model_fn=model_fn, model_dir=FLAGS.checkpoint_path)
+
+    if FLAGS.steps is None:
+        FLAGS.steps = 50000 // FLAGS.batch_size
+
+    results = classifier.evaluate(
+        input_fn=input_fn, steps=FLAGS.steps,
+        hooks=[hook(every_n_steps=FLAGS.log_every_n_steps)])
+
+    print("| {:5d} Samples    | Top-1       | Top-5       | Size   |".format(
+          FLAGS.batch_size * FLAGS.steps))
+    print("|------------------|-------------|-------------|--------|")
+    for (i, model_name) in enumerate(FLAGS.model_name.split(',')):
+        print("| {:16s} | {:6.3f}      | {:6.3f}      | {:5.1f}M |".format(
+              model_name,
+              100 * (1 - results["%dTop1" % i]),
+              100 * (1 - results["%dTop5" % i]),
+              results["%dSize" % i] / 10e5))
+
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ examples/evaluate_imagenet_all.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+CUDA_VISIBLE_DEVICES=0 screen -dLm bash -c "echo 0; \
+python evaluate_imagenet.py --model_name=ResNet50 --eval_image_size=224 --normalize=1; \
+python evaluate_imagenet.py --model_name=ResNet101 --eval_image_size=224 --normalize=1; \
+python evaluate_imagenet.py --model_name=ResNet152 --eval_image_size=224 --normalize=1; \
+python evaluate_imagenet.py --model_name=ResNeXt101c64 --eval_image_size=224 --normalize=3; \
+python evaluate_imagenet.py --model_name=Inception3 --eval_image_size=299 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet100 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet35v2 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet50v2 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet140v2 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=DenseNet121 --eval_image_size=224 --normalize=3; \
+python evaluate_imagenet.py --model_name=VGG19 --eval_image_size=224 --normalize=1; \
+python evaluate_imagenet.py --model_name=NASNetAlarge --eval_image_size=331 --normalize=2 --batch_size=100"
+
+CUDA_VISIBLE_DEVICES=1 screen -dLm bash -c "echo 1; \
+python evaluate_imagenet.py --model_name=ResNet50v2 --eval_image_size=299 --normalize=2; \
+python evaluate_imagenet.py --model_name=ResNet101v2 --eval_image_size=299 --normalize=2; \
+python evaluate_imagenet.py --model_name=ResNet152v2 --eval_image_size=299 --normalize=2; \
+python evaluate_imagenet.py --model_name=Inception4 --eval_image_size=299 --normalize=2; \
+python evaluate_imagenet.py --model_name=InceptionResNet2 --eval_image_size=299 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet75 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet75v2 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet100v2 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet130v2 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=DenseNet169 --eval_image_size=224 --normalize=3; \
+python evaluate_imagenet.py --model_name=VGG16 --eval_image_size=224 --normalize=1; \
+python evaluate_imagenet.py --model_name=PNASNetlarge --eval_image_size=331 --normalize=2 --batch_size=100"
+
+CUDA_VISIBLE_DEVICES=2 screen -dLm bash -c "echo 2; \
+python evaluate_imagenet.py --model_name=ResNet200v2 --eval_image_size=224 --normalize=3; \
+python evaluate_imagenet.py --model_name=ResNeXt50 --eval_image_size=224 --normalize=3; \
+python evaluate_imagenet.py --model_name=ResNeXt101 --eval_image_size=224 --normalize=3; \
+python evaluate_imagenet.py --model_name=WideResNet50 --eval_image_size=224 --normalize=5; \
+python evaluate_imagenet.py --model_name=Inception1 --eval_image_size=224 --normalize=4; \
+python evaluate_imagenet.py --model_name=Inception2 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet25 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=MobileNet50 --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=DenseNet201 --eval_image_size=224 --normalize=3; \
+python evaluate_imagenet.py --model_name=NASNetAmobile --eval_image_size=224 --normalize=2; \
+python evaluate_imagenet.py --model_name=SqueezeNet --eval_image_size=224 --normalize=4"
+
+cat screenlog.0 | grep "^|" | sort | uniq
